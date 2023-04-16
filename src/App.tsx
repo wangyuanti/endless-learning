02c87370e3fee638ab62842b2b5ef10b9777eaf2
wangyuanti1 <wangyuanti@163.com>
2023-04-17 00:22:04 +0800

    测试

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>2</div>
+        <div></div>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
